06b0ae0f7175447551916106bc0ed2c2dc72740e
SephirothZ87 <sephiroth.zhong@gmail.com>
2016-01-15 18:22:53 +0800

    1

---
+++ Client/js/index.js
@@ -0,0 +1,43 @@
+(function() {
+    console.log("build 0001");
+
+    var socket;
+
+    $(document).ready(function() {
+        $("#button").click(function() {
+            console.log("button be clicked");
+
+            // this.socket = io.connect('192.168.21.154:3000');
+            socket = io.connect('192.168.21.154:3000');
+
+
+            // $(window).keydown(function(event) {
+            //     console.log(event);
+            //     socket.emit('keydown', event);
+            // });
+
+
+            $(window).keydown(function(event) {
+            	if(event.keyCode == 37||event.keyCode == 38||event.keyCode == 39||event.keyCode == 40){
+                console.log(event);
+                // socket.emit('keydown', event);
+                socket.emit('keydown',event.keyCode);
+            	}
+            });
+        });
+
+        $("#button2").click(function() {
+            console.log("button be clicked");
+
+            socket.emit('keydown', {'a':1,'b':2});
+        });
+
+
+            // $(window).keydown(function(event) {
+            // 	if(event.keyCode == 37||event.keyCode == 38||event.keyCode == 39||event.keyCode == 40){
+            //     console.log(event);
+            //     // socket.emit('keydown', event);
+            // 	}
+            // });
+    });
+})();
