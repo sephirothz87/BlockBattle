685727c4ea3d725a779e4bf41171ca417b6ed9e6
sephirothz87 <sephiroth.zhong@gmail.com>
2016-01-27 00:45:59 +0800

    temp js中的延时执行并不好用

--- Client/js/index.js
+++ Client/js/index.js
@@ -4,11 +4,11 @@
     var socket;
 
     $(document).ready(function() {
-        $("#button").click(function() {
+        $("#join_game").click(function() {
             console.log("button be clicked");
 
             // this.socket = io.connect('192.168.21.154:3000');
-            socket = io.connect('192.168.21.154:3000');
+            socket = io.connect('192.168.1.102:3000');
 
 
             // $(window).keydown(function(event) {
@@ -18,26 +18,33 @@
 
 
             $(window).keydown(function(event) {
-            	if(event.keyCode == 37||event.keyCode == 38||event.keyCode == 39||event.keyCode == 40){
-                console.log(event);
-                // socket.emit('keydown', event);
-                socket.emit('keydown',event.keyCode);
-            	}
+                if (event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40) {
+                    // console.log(event);
+                    // socket.emit('keydown', event);
+                    socket.emit('keydown', event.keyCode);
+                }
+
+                socket.on('frame', function(o) {
+                    console.log(o);
+                });
             });
         });
 
         $("#button2").click(function() {
             console.log("button be clicked");
 
-            socket.emit('keydown', {'a':1,'b':2});
+            socket.emit('keydown', {
+                'a': 1,
+                'b': 2
+            });
         });
 
 
-            // $(window).keydown(function(event) {
-            // 	if(event.keyCode == 37||event.keyCode == 38||event.keyCode == 39||event.keyCode == 40){
-            //     console.log(event);
-            //     // socket.emit('keydown', event);
-            // 	}
-            // });
+        // $(window).keydown(function(event) {
+        //  if(event.keyCode == 37||event.keyCode == 38||event.keyCode == 39||event.keyCode == 40){
+        //     console.log(event);
+        //     // socket.emit('keydown', event);
+        //  }
+        // });
     });
 })();
