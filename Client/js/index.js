b8d26bf401a94d95f9d15e688939296a54d6549a
SephirothZ87 <sephiroth.zhong@gmail.com>
2016-02-05 16:56:51 +0800

    socket计算内容完全实装，html canvas描画需要实装进去

--- Client/js/index.js
+++ Client/js/index.js
@@ -2,6 +2,12 @@
     console.log("build 0001");
 
     var socket;
+    //在线用户
+    var onlineUsers = {};
+
+    genUid = function() {
+        return new Date().getTime() + "" + Math.floor(Math.random() * 899 + 100);
+    }
 
     $(document).ready(function() {
         $("#button").click(function() {
@@ -10,12 +16,17 @@
             // this.socket = io.connect('192.168.21.154:3000');
             socket = io.connect('192.168.21.154:3000');
 
-
             // $(window).keydown(function(event) {
             //     console.log(event);
             //     socket.emit('keydown', event);
             // });
 
+            var userid = genUid();
+            console.log(userid);
+            var obj_user = {
+                userid: userid
+            };
+            socket.emit('join_game', obj_user);
 
             $(window).keydown(function(event) {
                 if (event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40) {
@@ -25,17 +36,33 @@
                 }
             });
 
-            socket.on('message', function(o) {
-                console.log(o);
+            socket.on('join_game', function(obj) {
+                console.log(obj);
+                onlineUsers = obj.onlineUsers;
+            });
+
+            socket.on('exit_game', function(obj) {
+                console.log(obj);
+                onlineUsers = obj.onlineUsers;
+            });
+
+            socket.on('message', function(obj) {
+                console.log(obj);
             });
         });
 
         $("#button2").click(function() {
-            console.log("button be clicked");
-            socket.emit('keydown', {
-                'a': 1,
-                'b': 2
-            });
+            // console.log("button be clicked");
+            // socket.emit('keydown', {
+            //     'a': 1,
+            //     'b': 2
+            // });
+
+            // var html = "<div id='' class='battle_block'></div>";
+            var canvas = document.getElementById('battle_area');
+            var context = canvas.getContext("2d");
+            context.fillStyle = "red";
+            context.fillRect(50, 50, 70, 70);
         });
 
         $("#button3").click(function() {
--- Server/index.js
+++ Server/index.js
@@ -11,58 +11,101 @@ var onlineUsers = {};
 //当前在线人数
 var onlineCount = 0;
 
+var ii = 0;
+
+myInterval = function() {
+    // ii++;
+    // console.log(ii);
+    // io.emit('message', ii);
+    io.emit('message', onlineUsers);
+}
+
+getRandomColor = function() {
+    return '#' + (Math.random() * 0xffffff << 0).toString(16);
+}
+
 io.on('connection', function(socket) {
     console.log('a user connected');
 
     socket.on('keydown', function(obj) {
         console.log("on keydown");
         console.log(obj);
+
+        switch (obj) {
+            case 37:
+                if (onlineUsers[socket.name]['x'] > 0) {
+                    onlineUsers[socket.name]['x'] -= 1;
+                }
+                break;
+            case 38:
+                if (onlineUsers[socket.name]['y'] > 0) {
+                    onlineUsers[socket.name]['y'] -= 1;
+                }
+                break;
+            case 39:
+                if (onlineUsers[socket.name]['x'] < 180) {
+                    onlineUsers[socket.name]['x'] += 1;
+                }
+                break;
+            case 40:
+                if (onlineUsers[socket.name]['y'] < 180) {
+                    onlineUsers[socket.name]['y'] += 1;
+                }
+                break;
+        }
     });
 
     //监听新用户加入
-    socket.on('login', function(obj) {
+    socket.on('join_game', function(obj) {
         //将新加入用户的唯一标识当作socket的名称，后面退出的时候会用到
         socket.name = obj.userid;
 
+        console.log();
+
         //检查在线列表，如果不在里面就加入
         if (!onlineUsers.hasOwnProperty(obj.userid)) {
-            onlineUsers[obj.userid] = obj.username;
+            // onlineUsers[obj.userid] = obj.userid;
+            onlineUsers[obj.userid] = {
+                userid: socket.name,
+                color: getRandomColor(),
+                x: parseInt(Math.random() * 180),
+                y: parseInt(Math.random() * 180)
+            };
             //在线人数+1
             onlineCount++;
         }
 
         //向所有客户端广播用户加入
-        io.emit('login', {
+        io.emit('join_game', {
             onlineUsers: onlineUsers,
             onlineCount: onlineCount,
             user: obj
         });
-        console.log(obj.username + '加入了游戏');
+        console.log(obj.userid + '加入了游戏');
     });
 
     //监听用户退出
     socket.on('disconnect', function() {
         //将退出的用户从在线列表中删除
-        // if (onlineUsers.hasOwnProperty(socket.name)) {
-        //     //退出用户的信息
-        //     var obj = {
-        //         userid: socket.name,
-        //         username: onlineUsers[socket.name]
-        //     };
+        if (onlineUsers.hasOwnProperty(socket.name)) {
+            //退出用户的信息
+            var obj = {
+                userid: socket.name
+            };
 
-        //     //删除
-        //     delete onlineUsers[socket.name];
-        //     //在线人数-1
-        //     onlineCount--;
+            //删除
+            delete onlineUsers[socket.name];
+            //在线人数-1
+            onlineCount--;
 
-        //     //向所有客户端广播用户退出
-        //     io.emit('logout', {
-        //         onlineUsers: onlineUsers,
-        //         onlineCount: onlineCount,
-        //         user: obj
-        //     });
-        //     console.log(obj.username + '退出了游戏');
-        // }
+            //向所有客户端广播用户退出
+            io.emit('exit_game', {
+                onlineUsers: onlineUsers,
+                onlineCount: onlineCount,
+                user: obj
+            });
+            console.log(obj.userid + '退出了游戏');
+        }
         console.log('disconnect');
     });
 
@@ -75,16 +118,8 @@ io.on('connection', function(socket) {
 
 });
 
-var ii = 0;
-
-myInterval = function() {
-    ii++;
-    console.log(ii);
-    io.emit('message', ii);
-}
-
 http.listen(3000, function() {
     console.log('listening on *:3000');
 
-    setInterval(myInterval, 1000); //1000为1秒钟
+    setInterval(myInterval, 5000); //1000为1秒钟
 });
